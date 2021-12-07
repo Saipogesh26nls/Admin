a3539154c988056f4f7d789fe9aad9b34bb9c829
Sathish J <sathish2301ece@gmail.com>
2021-12-07 17:52:34 +0530

    07-Dec-2021

--- Controllers/BOMController.cs
+++ Controllers/BOMController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Admin.Models;
 
 namespace Admin.Controllers
 {
     public class BOMController : Controller
     {
         // GET: BOM
-        public ActionResult BOM_AddList()
+        public ActionResult BOM_Add_Data()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult P_Description(BOMFields newuser)
+        {
+            BOM_Insert dblogin = new BOM_Insert();
+            var values = dblogin.P_Description( newuser.SP_Part_No);
+            ViewBag.ItemQ = values;
+            BOM_Add_Data();
+            return View("BOM_Add_Data", values);
         }
     }
 }
---
+++ Models/BOM_Insert.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Admin.Models;
+
+namespace Admin.Models
+{
+    public class BOM_Insert
+    {
+        public List<BOMFields> P_Description(string cSP_Part_No)
+        {
+            List<BOMFields> ItemQm = new List<BOMFields>();
+            DB_Con_Str OCon = new DB_Con_Str();
+            string ConString = OCon.DB_Data();
+            SqlConnection Con = new SqlConnection(ConString);
+            Con.Open();
+            string cmd1="select P_Description from Product_Master where P_Part_No = '" + cSP_Part_No+ "'";
+            SqlCommand SqlCmd1 = new SqlCommand(cmd1, Con);
+            SqlDataReader dr = SqlCmd1.ExecuteReader();
+
+            while (dr.Read())
+            {
+                ItemQm.Add(new BOMFields
+                {
+                    SP_Description = dr["P_Description"].ToString()
+                }
+                );
+            }
+            Con.Close();
+            return ItemQm;
+        }
+    }
+}
